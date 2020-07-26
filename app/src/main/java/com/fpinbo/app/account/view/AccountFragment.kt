85240ada1922970ee904aaba8d7dde75b480c28a
Daniele Campogiani <daniele.campogiani@gmail.com>
2020-07-26 15:07:20 +0200

    chore: use latest stables

--- app/src/main/java/com/fpinbo/app/account/view/AccountFragment.kt
+++ app/src/main/java/com/fpinbo/app/account/view/AccountFragment.kt
@@ -3,7 +3,6 @@ package com.fpinbo.app.account.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.TransitionManager
 import com.fpinbo.app.R
 import com.fpinbo.app.account.*
 import com.fpinbo.app.account.inject.AccountModule
@@ -55,7 +55,7 @@ class AccountFragment : Fragment() {
 
         viewModel.state.observe(viewLifecycleOwner, Observer {
 
-            TransitionManager.beginDelayedTransition(root, MaterialFadeThrough.create())
+            TransitionManager.beginDelayedTransition(root, MaterialFadeThrough())
 
             exhaustive..when (it) {
                 is Logged -> showLoggedUser(it.user)
